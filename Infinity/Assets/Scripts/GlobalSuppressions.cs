009c06f419945d768fe885559ff7e1909580b8d6
blenderfreaky <blendcrafterxiv@gmail.com>
2020-09-23 04:39:51 +0200

    Error supressions

---
+++ Infinity/Assets/Scripts/GlobalSuppressions.cs
@@ -0,0 +1,29 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+#region Auto-Generated PlayerInputActions.cs
+
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "Generated Code", Scope = "member", Target = "~F:Assets.Players.PlayerInputActions.PlayerActions.m_Wrapper")]
+[assembly: SuppressMessage("Design", "RCS1169:Make field read-only.", Justification = "Generated Code", Scope = "member", Target = "~F:Assets.Players.PlayerInputActions.PlayerActions.m_Wrapper")]
+[assembly: SuppressMessage("Design", "RCS1169:Make field read-only.", Justification = "Generated Code", Scope = "member", Target = "~F:Assets.Players.PlayerInputActions.UIActions.m_Wrapper")]
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "Generated Code", Scope = "member", Target = "~F:Assets.Players.PlayerInputActions.UIActions.m_Wrapper")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Generated Code", Scope = "member", Target = "~P:Assets.Players.PlayerInputActions.asset")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Generated Code", Scope = "member", Target = "~P:Assets.Players.PlayerInputActions.PlayerActions.enabled")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Generated Code", Scope = "member", Target = "~P:Assets.Players.PlayerInputActions.UIActions.enabled")]
+[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "Generated Code", Scope = "type", Target = "~T:Assets.Players.PlayerInputActions")]
+[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "Generated Code", Scope = "type", Target = "~T:Assets.Players.PlayerInputActions.IPlayerActions")]
+[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "Generated Code", Scope = "type", Target = "~T:Assets.Players.PlayerInputActions.IUIActions")]
+[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "Generated Code", Scope = "type", Target = "~T:Assets.Players.PlayerInputActions.PlayerActions")]
+[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "Generated Code", Scope = "type", Target = "~T:Assets.Players.PlayerInputActions.UIActions")]
+
+#endregion Auto-Generated PlayerInputActions.cs
+
+[assembly: SuppressMessage("Style", "IDE0029:Use coalesce expression", Justification = "Unity still doesn't support null coalescing")]
+[assembly: SuppressMessage("Simplification", "RCS1084:Use coalesce expression instead of conditional expression.", Justification = "Unity still doesn't support null coalescing")]
+[assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Unity still doesn't support null coalescing")]
+[assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "Unity messages falsely trigger this")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Unity messages falsely trigger this")]
