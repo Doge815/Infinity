61abd76eae0252704cbe3366e98133d178b68120
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2020-09-23 04:43:38 +0200

    improved visibility check

--- Infinity/Assets/Scripts/World.cs
+++ Infinity/Assets/Scripts/World.cs
@@ -5,6 +5,12 @@ namespace Assets
 {
     public class World
     {
+        public static World ActiveWorld;
         public Dictionary<Vector3Int, Chunk> Chunks;
+
+        public World()
+        {
+            Chunks = new Dictionary<Vector3Int, Chunk>();
+        }
     }
 }
