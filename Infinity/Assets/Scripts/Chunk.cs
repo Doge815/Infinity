55b373835f41d94813b9be0a9ae17b7307ad66fd
Doge815 <42283470+Johannes7683@users.noreply.github.com>
2020-09-23 03:01:37 +0200

    basic draw

--- Infinity/Assets/Scripts/Chunk.cs
+++ Infinity/Assets/Scripts/Chunk.cs
@@ -50,24 +50,51 @@ namespace Assets
                 {
                     for (int z = 0; z < Size; z++)
                     {
-                        BlockType block = Map[x, y, z];
-                        if (block.ID == 0) continue;
-                        DrawBlock(new Vector3(x, y, z), block);
+                        int block = Map[x, y, z];
+                        if (block == 0) continue;
+                        DrawBlock(x, y, z, block);
                     }
                 }
             }
 
+            mesh.vertices = verts.ToArray();
+            mesh.triangles = tris.ToArray();
+            mesh.RecalculateNormals();
+
         }
 
-        void DrawBlock(Vector3 start, Block block)
+        void DrawBlock(int x, int y, int z, int block)
         {
+            Vector3 pos = new Vector3(x, y, z);
             Vector3 offset1, offset2;
+            if(IsInvisible(x, y-1, z))
+            {
+                offset1 = Vector3.right;
+                offset2 = Vector3.back;
+                DrawIt(pos, offset1, offset2, block);
+            }
         }
 
-        bool NeedDraw(Vector3 pos)
+        void DrawIt(Vector3 pos, Vector3 o1, Vector3 o2, int block)
         {
-            if (pos[0] < 0 || pos[1] < 0 || pos[2] < 0) return false;
-            return true;
+            verts.Add(pos);
+            verts.Add(pos + o1);
+            verts.Add(pos + o2);
+            verts.Add(pos + o1 + o2);
+
+            int index = verts.Count;
+            tris.Add(index + 0);
+            tris.Add(index + 1);
+            tris.Add(index + 2);
+            tris.Add(index + 3);
+            tris.Add(index + 2);
+            tris.Add(index + 1);
+        }
+
+        bool IsInvisible(int x, int y, int z)
+        {
+            if (x < 0 || y < 0 || z < 0 || x >= Size || y >= Size || z >= Size) return true;
+            return Map[x,y,z] == 0;
         }
 
 
